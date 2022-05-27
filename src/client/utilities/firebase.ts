4e8ea6d61f7e39338433b0c804b11cfe136d8eda
P-Chan <hello@0x50.io>
2022-05-27 20:08:06 +0900

    feat: auth の設定を追加

---
+++ src/client/utilities/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApps, FirebaseApp } from 'firebase/app'
+import { getAuth, connectAuthEmulator, Auth } from 'firebase/auth'
+
+import { emulators } from '~/firebase.json'
+import { globalSettings } from '@shared/utilities'
+
+let app: FirebaseApp
+let auth: Auth
+
+if (typeof window !== 'undefined' && !getApps().length) {
+  app = initializeApp(globalSettings.firebase)
+  auth = getAuth(app)
+
+  if (process.env.NODE_ENV === 'development') {
+    connectAuthEmulator(auth, `http://localhost:${emulators.auth.port}`)
+  }
+}
+
+export { auth }
